6987744df387810c9311fcea19c1fdca749de911
Ananda Dwi Rahmawati <anandadwirahmawati@acpcomputer.edu.sg>
2025-10-13 14:45:56 +0800

    Normalize formatting (terraform fmt output)

--- backend.tf
+++ backend.tf
@@ -1,5 +1,3 @@
 terraform {
-  # GCS backend configuration. Bucket & prefix are supplied via terraform init -backend-config flags.
-  # Workspaces (dev, prod) will create separate state objects under the same prefix automatically (env:<workspace> naming).
   backend "gcs" {}
 }
