423745727abd7a096101054ee5c8088fb27556cb
Ananda Dwi Rahmawati <anandadwirahmawati@acpcomputer.edu.sg>
2025-10-13 11:56:22 +0800

    add files

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 5.0"
-    }
-  }
-}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 module "network" {
   source                = "./modules/network"
   project_id            = var.project_id
